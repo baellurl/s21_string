4669356f5d4fb9fcdd5b13103748326040143b2d
Dastan <dastan_9292@internet.ru>
2025-06-22 18:44:35 +0000

    ----------

---
+++ s21_memset.c
@@ -0,0 +1,18 @@
+#include "s21_string.h"
+
+
+// Копирует символ c (беззнаковый тип) в первые n символов строки
+// на которую указывает аргумент str.
+
+void *s21_memset(void *str, int __c, s21_size_t __n){
+
+unsigned char *_str = (unsigned char *)str;
+
+for(s21_size_t i = 0; i < __n; i++){
+    
+    _str[i] = (unsigned char ) __c;
+}
+
+return _str;
+
+}
