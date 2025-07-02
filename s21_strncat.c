8ff80afe41cf85543bdcea2f7f686f93c8c3e7fa
Dastan <dastan_9292@internet.ru>
2025-07-02 23:58:45 +0000

    __strncmp __strncpy __strncat

--- s21_strncat.c
+++ s21_strncat.c
@@ -1,5 +1,8 @@
 #include "s21_string.h"
 
+//Добавляет строку, на которую указывает src, в конец строки,
+// на которую указывает dest, длиной до n символов.
+
 char *s21_strncat(char *dest, const char *src, s21_size_t __n) {
 
   s21_size_t i = 0, j = 0;
